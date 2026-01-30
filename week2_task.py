db144b748c7762014180f7d953bf93bb146eb0e1
DAVID ARIKPO <0arikd65@solent.ac.uk>
2026-01-30 15:49:07 +0000

    week 2

---
+++ week2_task.py
@@ -0,0 +1,35 @@
+age = int(input("How old are you? "))
+if age <= 0:
+    print("Sorry, you are not old enough.")
+my_age = 25
+if my_age <= 0:
+    print("Sorry, you are not old enough.")
+day = int(input("enter the day"))
+if day <= 0:
+    print("Sorry, you are not old enough.")
+month = int(input("enter the month:"))
+if month <= 0:
+    print("Sorry, you are not old enough.")
+if day== 1 and month == 4:
+    print("it is april fools day")
+if age < 18:
+    print(" you are too young to buy alcohol.")
+else:
+    print(" you are too young to buy alcohol.")
+age = int(input("how old are you"))
+if age <= 0:
+    print("wow, people start using computer early")
+
+my_age = 20
+if my_age == my_age:
+    print("hey! we are the same age!")
+day = int( input("enter the day:"))
+month = int(input("enter the month: "))
+
+if day == 1 and month == 4:
+    print ("it is april fools day")
+
+if age < 18:
+    ("you are too young to buy alcohol.")
+else:
+    print( "you are old enough to buy alcohol.")
