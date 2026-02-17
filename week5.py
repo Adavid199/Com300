75b141226468bd52ab3cbd97d2ca08db2f8641d8
DAVID ARIKPO <0arikd65@solent.ac.uk>
2026-02-17 12:54:26 +0000

    week5

---
+++ week5.py
@@ -0,0 +1,10 @@
+capacity = 30
+non_student = 28
+room_booked = False
+computer_required = True
+nun_computer = 25
+
+if room_booked:
+    print("room is already booked")
+elif nun_computer > capacity :
+    print("not enough space in the room")
